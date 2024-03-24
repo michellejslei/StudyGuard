d45c0616ccc4f952132d7c63625ec9b8d34af240
colemantatiana <146880032+colemantatiana@users.noreply.github.com>
2024-03-23 21:15:40 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Entrance from './Entrance.jsx'
+import './index.css'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Entrance />  */}
+    <App />
+  </React.StrictMode>,
+)
