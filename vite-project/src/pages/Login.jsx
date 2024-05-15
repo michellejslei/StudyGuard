828ef52ac4d704a0c62d24dcfb493427ca1b6692
Michelle <michellejslei@gmail.com>
2024-05-15 14:45:39 -0700

    sign in page initialized

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { DarkGridAuth } from '../components/Signin'
 
 const Login = () => {
   return (
     <div className="bg-wave-pattern h-screen bg-cover bg-center flex flex-col z-0 w-full relative">
+      <div className="z-10 w-full">
+        <DarkGridAuth />
+      </div>
     </div>
   )
 }
