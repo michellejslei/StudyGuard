7f08b66fa2e3a255d5a6e53e0a45b89b2c0140eb
Michelle <michellejslei@gmail.com>
2024-05-15 14:30:10 -0700

    about page initialized

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="bg-wave-pattern h-screen bg-cover bg-center flex flex-col z-0 w-full relative">
+      <div className="text-center z-10 relative px-10 py-60 lg:max-w-4xl mx-auto">
+        <h1 class="text-3xl font-extrabold text-white">
+        Staying focused while studying alone can be hard, especially when there are so many distractions! 
+        StudyGuard is an ML study buddy that will keep you focused the way your mom would have kept you accountable in high school!
+        </h1>
+      </div>
+    </div>
   )
 }
 
--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="bg-wave-pattern h-screen bg-cover bg-center flex flex-col z-0 w-full relative">
+    </div>
   )
 }
 
